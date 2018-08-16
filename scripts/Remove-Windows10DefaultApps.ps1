4318c31e547a45c27729eefd27ba3e152b660175
Hilton <mahilton@volusia.k12.fl.us>
2018-08-16 10:08:21 -0400

    Initial 1.0

--- scripts/Remove-Windows10DefaultApps.ps1
+++ scripts/Remove-Windows10DefaultApps.ps1
@@ -1,126 +1,69 @@
-# Description:
-# This script removes apps that come by default in Windows 10 Enterprise Edition 1803
-
-Write-Output "Elevating privileges for this process"
-do {} until (Elevate-Privileges SeTakeOwnershipPrivilege)
+<#
+    Script name: 	Remove-Windows10DefaultApps.ps1
+	Version:     	0.1
+	Purpose:		Remove unwanted default apps from a Windows 10 1803 Enterprise install
+    Author:      	Matt Hilton
+    Contact:     	matthilt@gmail.com
+    DateCreated: 	2018-08-16
+    LastUpdate:  	2018-08-16
+#>
 
 Write-Output "Uninstalling default apps"
 $apps = @(
 	# default Windows 10 apps
-	"Microsoft.3DBuilder"
-	"Microsoft.Appconnector"
-	"Microsoft.BingFinance"
 	"Microsoft.BingNews"
-	"Microsoft.BingSports"
-	"Microsoft.BingTranslator"
 	"Microsoft.BingWeather"
-	#"Microsoft.FreshPaint"
+	#"Microsoft.DesktopAppInstaller"
+	"Microsoft.FreshPaint"
+	"Microsoft.GetHelp"
+	"Microsoft.Getstarted"
+	#"Microsoft.Messaging"
 	"Microsoft.Microsoft3DViewer"
 	"Microsoft.MicrosoftOfficeHub"
 	"Microsoft.MicrosoftSolitaireCollection"
-	"Microsoft.MicrosoftPowerBIForWindows"
-	"Microsoft.MinecraftUWP"
-	#"Microsoft.MicrosoftStickyNotes"
+	"Microsoft.MicrosoftStickyNotes"
+	#"Microsoft.MSPaint"
 	"Microsoft.NetworkSpeedTest"
-	"Microsoft.Office.OneNote"
-	#"Microsoft.OneConnect"
+	#"Microsoft.Office.OneNote"
+	"Microsoft.OneConnect"
 	"Microsoft.People"
 	"Microsoft.Print3D"
 	"Microsoft.SkypeApp"
+	#"Microsoft.StorePurchaseApp"
 	"Microsoft.Wallet"
-	#"Microsoft.Windows.Photos"
+	#"Microsoft.WebMediaExtensions"
+	"Microsoft.Windows.Photos"
 	"Microsoft.WindowsAlarms"
 	#"Microsoft.WindowsCalculator"
 	"Microsoft.WindowsCamera"
 	"microsoft.windowscommunicationsapps"
+	"Microsoft.WindowsFeedbackHub"
 	"Microsoft.WindowsMaps"
-	"Microsoft.WindowsPhone"
-	"Microsoft.WindowsSoundRecorder"
+	#"Microsoft.WindowsSoundRecorder"
 	#"Microsoft.WindowsStore"
 	"Microsoft.XboxApp"
+	"Microsoft.Xbox.TCUI"
 	"Microsoft.XboxGameOverlay"
+	"Microsoft.XboxGamingOverlay"
 	"Microsoft.XboxIdentityProvider"
 	"Microsoft.XboxSpeechToTextOverlay"
 	"Microsoft.ZuneMusic"
 	"Microsoft.ZuneVideo"
-    
-    
-	# Threshold 2 apps
-	"Microsoft.CommsPhone"
-	"Microsoft.ConnectivityStore"
-	"Microsoft.GetHelp"
-	"Microsoft.Getstarted"
-	"Microsoft.Messaging"
-	"Microsoft.Office.Sway"
-	"Microsoft.OneConnect"
-	"Microsoft.WindowsFeedbackHub"
-
-	# Creators Update apps
-	"Microsoft.Microsoft3DViewer"
-	#"Microsoft.MSPaint"
-
-	#Redstone apps
-	"Microsoft.BingFoodAndDrink"
-	"Microsoft.BingTravel"
-	"Microsoft.BingHealthAndFitness"
-	"Microsoft.WindowsReadingList"
-
 
 	# non-Microsoft
-	"9E2F88E3.Twitter"
-	"PandoraMediaInc.29680B314EFC2"
-	"Flipboard.Flipboard"
-	"ShazamEntertainmentLtd.Shazam"
-	"king.com.CandyCrushSaga"
-	"king.com.CandyCrushSodaSaga"
-	"king.com.*"
-	"ClearChannelRadioDigital.iHeartRadio"
-	"4DF9E0F8.Netflix"
-	"6Wunderkinder.Wunderlist"
-	"Drawboard.DrawboardPDF"
-	"2FE3CB00.PicsArt-PhotoStudio"
-	"D52A8D61.FarmVille2CountryEscape"
-	"TuneIn.TuneInRadio"
-	"GAMELOFTSA.Asphalt8Airborne"
-	#"TheNewYorkTimes.NYTCrossword"
-	"DB6EA5DB.CyberLinkMediaSuiteEssentials"
-	"Facebook.Facebook"
-	"flaregamesGmbH.RoyalRevolt2"
-	"Playtika.CaesarsSlotsFreeCasino"
-	"A278AB0D.MarchofEmpires"
-	"KeeperSecurityInc.Keeper"
-	"ThumbmunkeysLtd.PhototasticCollage"
-	"XINGAG.XING"
-	"89006A2E.AutodeskSketchBook"
-	"D5EA27B7.Duolingo-LearnLanguagesforFree"
-	"46928bounde.EclipseManager"
-	"ActiproSoftwareLLC.562882FEEB491" # next one is for the Code Writer from Actipro Software LLC
-	"DolbyLaboratories.DolbyAccess"
-	"SpotifyAB.SpotifyMusic"
-	"A278AB0D.DisneyMagicKingdoms"
-	"WinZipComputing.WinZipUniversal"
-
-
-	# apps which cannot be removed using Remove-AppxPackage
-	#"Microsoft.BioEnrollment"
-	#"Microsoft.MicrosoftEdge"
-	#"Microsoft.Windows.Cortana"
-	#"Microsoft.WindowsFeedback"
-	#"Microsoft.XboxGameCallableUI"
-	#"Microsoft.XboxIdentityProvider"
-	#"Windows.ContactSupport"
+	"*AdobePhotoshopExpress*" #Adobe Photoshop Express
+	"*ActiproSoftwareLLC*" #Code Writer
+	"*Duolingo*" #Duolingo
+	"*EclipseManager*"
 )
 
 foreach ($app in $apps) {
 	Write-Output "Trying to remove $app"
-
-	Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage -AllUsers
-
-	Get-AppXProvisionedPackage -Online |
-		Where-Object DisplayName -EQ $app |
-		Remove-AppxProvisionedPackage -Online
+	Get-AppxPackage -Name $app | Remove-AppxPackage
+	Get-AppXProvisionedPackage -Online | Where-Object DisplayName -EQ $app | Remove-AppxProvisionedPackage -Online
 }
 
-# Prevents "Suggested Applications" returning
-force-mkdir "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Cloud Content"
-Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Cloud Content" "DisableWindowsConsumerFeatures" 1
+foreach ($app in $apps) {
+	Write-Output "Trying to remove $app from all users"
+	Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage -AllUsers
+}
---
+++ scripts/Set-RegistrySettings.ps1
@@ -0,0 +1,83 @@
+<#
+    Script name: 	Set-RegistrySettings.ps1
+	Version:     	1803.1.0
+	Purpose:		Sets my desired registry settings for a default install
+    Author:      	Matt Hilton
+    Contact:     	matthilt@gmail.com
+    DateCreated: 	2018-08-16
+    LastUpdate:  	2018-08-16
+#>
+
+$KeyCUMicrosoft = "HKCU:\SOFTWARE\Microsoft"
+Set-ItemProperty -Path "$KeyCUMicrosoft\Gamebar" -Name "ShowStartupPanel" -Value 0 # Disable GameBar Tips
+
+$KeyCUCurrentVersion = "$KeyCUMicrosoft\Windows\CurrentVersion"
+If (-Not (Test-Path "$KeyCUCurrentVersion\AdvertisingInfo")) { New-Item -Path $KeyCUCurrentVersion -Name "AdvertisingInfo" | Out-Null}
+Set-ItemProperty -Path "$KeyCUCurrentVersion\AdvertisingInfo" -Name Enabled -Type DWord -Value 0 # Disable Let apps use my advertising ID
+Set-ItemProperty -Path "$KeyCUCurrentVersion\AppHost" -Name EnableWebContentEvaluation -Type DWord -Value 0 # Disable SmartScreen Filter for Store Apps
+
+$KeyCUExplorer = "$KeyCUCurrentVersion\Explorer"
+Set-ItemProperty -Path $KeyCUExplorer -Name EnableAutoTray -Value 0 # Show all icons in tray
+Set-ItemProperty -Path $KeyCUExplorer -Name ShowRecent -Type DWord -Value 0 # Disable Quick Access: Recent Files
+Set-ItemProperty -Path $KeyCUExplorer -Name ShowFrequent -Type DWord -Value 0 # Disable Quick Access: Frequent Folders
+
+$KeyCUAdvanced = "$KeyCUExplorer\Advanced"
+Set-ItemProperty -Path $KeyCUAdvanced -Name AutoCheckSelect -Value 0 # Disable check boxes
+Set-ItemProperty -Path $KeyCUAdvanced -Name HideFileExt 0 # Show File Extensions
+Set-ItemProperty -Path $KeyCUAdvanced -Name LaunchTo -Type DWord -Value 1 # Change Explorer home screen back to "This PC"
+Set-ItemProperty -Path $KeyCUAdvanced -Name MMTaskbarMode -Value 2 # Show taskbar buttons on all taskbars
+Set-ItemProperty -Path $KeyCUAdvanced -Name NavPaneExpandToCurrentFolder -Value 1 # Expand This PC to open folder in Nav Pane
+Set-ItemProperty -Path $KeyCUAdvanced -Name NavPaneShowAllFolders -Value 1 # Show all folders in Nav Pane in Explorer
+Set-ItemProperty -Path $KeyCUAdvanced -Name ShowTaskViewButton -Value 0 # Hide task view button on taskbar
+Set-ItemProperty -Path $KeyCUAdvanced -Name TaskbarGlomLevel 0 # Taskbar Combine Icons
+Set-ItemProperty -Path $KeyCUAdvanced -Name TaskbarSmallIcons 1 # Taskbar Size Small
+
+Set-ItemProperty -Path "$KeyCUAdvanced\People" -Name PeopleBand -Type DWord -Value 0 # Turn off People in Taskbar
+
+Set-ItemProperty -Path "$KeyCUCurrentVersion\GameDVR" -Name AppCaptureEnabled -Type DWord -Value 0 # Disable Xbox Gamebar
+Set-ItemProperty -Path "HKCU:\System\GameConfigStore" -Name GameDVR_Enabled -Type DWord -Value 0 # Disable Xbox Gamebar
+
+$KeySearch = "$KeyCUCurrentVersion\Search"
+Set-ItemProperty -Path $KeySearch -Name "BingSearchEnabled" -Value 0 # Disable Bing Search
+Set-ItemProperty -Path $KeySearch -Name SearchboxTaskbarMode -Type DWord -Value 0 # TaskBar Hide Cortana Search
+
+$KeyLMWiFi = "HKLM:\Software\Microsoft\PolicyManager\default\WiFi\"
+$KeyLMAllowWiFiHotSpotReporting = "$KeyLMWiFi\AllowWiFiHotSpotReporting"
+If (-Not (Test-Path $KeyLMAllowWiFiHotSpotReporting)) { New-Item -Path $KeyLMWiFi -Name "AllowWiFiHotSpotReporting" | Out-Null}
+Set-ItemProperty -Path $KeyLMAllowWiFiHotSpotReporting -Name value -Type DWord -Value 0 # Disable Hotspot Sharing
+Set-ItemProperty -Path "$KeyLMWifi\AllowAutoConnectToWiFiSenseHotspots" -Name value -Type DWord -Value 0 # Disable Shared HotSpot Auto-Connect
+
+$KeyLMWindows = "HKLM:\SOFTWARE\Policies\Microsoft\Windows"
+If (-Not (Test-Path "$KeyLMWindows\Cloud Content")) { New-Item -Path $KeyLMWindows -Name "Cloud Content" -Force | Out-Null }
+New-ItemProperty -Path "$KeyLMWindows\Cloud Content" -Name "DisableWindowsConsumerFeatures" -Value 1 -PropertyType DWORD -Force| Out-Null # Prevents "Suggested Applications" returning
+If (-Not (Test-Path "$KeyLMWindows\Explorer")) { New-Item -Path $KeyLMWindows -Name Explorer -Force | Out-Null }
+New-ItemProperty -Path "$KeyLMWindows\Explorer" -Name "ShowRunasDifferentuserinStart" -Value 1 -PropertyType DWORD -Force| Out-Null #Add RunAs another user to Start Menu
+
+# Disable the Lock Screen (the one before password prompt - to prevent dropping the first character)
+If (-Not (Test-Path $KeyLMWindows\Personalization)) { New-Item -Path $KeyLMWindows -Name Personalization | Out-Null }
+Set-ItemProperty -Path $KeyLMWindows\Personalization -Name NoLockScreen -Type DWord -Value 1
+
+# Add photoviewer as open option for pictures
+New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT | Out-Null
+
+$KeyCRApplications = "HKCR:\Applications"
+$KeyCRPhotoviewer = "$KeyCRApplications\photoviewer.dll"
+$KeyCRShell = "$KeyCRPhotoviewer\shell"
+
+New-Item -Path $KeyCRApplications -Name "photoviewer.dll" -Force | Out-Null
+New-Item -Path $KeyCRPhotoviewer -Name "shell" -Force | Out-Null
+New-Item -Path $KeyCRShell -Name "open" -Force | Out-Null
+New-Item -Path "$KeyCRShell\open" -Name "command" -Force | Out-Null
+New-Item -Path "$KeyCRShell\open" -Name "DropTarget" -Force | Out-Null
+New-ItemProperty -Path "$KeyCRShell\open" -Name "MuiVerb" -Value "@photoviewer.dll,-3043" -PropertyType string -Force | Out-Null
+New-ItemProperty -Path "$KeyCRShell\open\command" -Name "(Default)" -Value '%SystemRoot%\System32\rundll32.exe "%ProgramFiles%\Windows Photo Viewer\PhotoViewer.dll", ImageView_Fullscreen %1' -PropertyType ExpandString -Force | Out-Null
+New-ItemProperty -Path "$KeyCRShell\open\DropTarget" -Name "Clsid" -Value "{FFE2A43C-56B9-4bf5-9A79-CC6D4285608A}" -PropertyType string -Force | Out-Null
+New-Item -Path "$KeyCRShell" -Name "print" -Force | Out-Null
+New-Item -Path "$KeyCRShell\print" -Name "command" -Force | Out-Null
+New-Item -Path "$KeyCRShell\print" -Name "DropTarget" -Force | Out-Null
+New-ItemProperty -Path "$KeyCRShell\print\command" -Name "(Default)" -Value '%SystemRoot%\System32\rundll32.exe "%ProgramFiles%\Windows Photo Viewer\PhotoViewer.dll", ImageView_Fullscreen %1' -PropertyType ExpandString -Force | Out-Null
+New-ItemProperty -Path "$KeyCRShell\print\DropTarget" -Name "Clsid" -Value "{60fd46de-f830-4894-a628-6fa81bc0190d}" -PropertyType string -Force | Out-Null
+
+Remove-PSDrive -Name HKCR
+
+Stop-Process -Name explorer
